23d846065746669687e7d94ce9425c17199600c6
DaniMasterLucky <danielsilvaloew@gmail.com>
2023-05-15 19:10:37 -0300

    condicionais

---
+++ intex.js
@@ -0,0 +1,69 @@
+// const a = 10
+// const b  = 20 
+
+// if(a < b){
+// console.log("B é maior que a")
+
+
+// }
+
+
+// const numero1 = Number(prompt("Digite um numero"))
+// const numero2 = Number(prompt("Digite outro numero"))
+
+
+// // function comparar(n1, n2) {
+// //     if (n1 > n2) {
+// //         console.log(`o ${n1} é maior que o ${n2}`)
+// // } else if (n1 < n2){
+// //         console.log(`o ${n1} é menor que o ${n2}`)
+
+// //     }else{
+// //         console.log(`O ${n1} é igual ao ${n2} `)
+
+// //     }
+
+//  switch
+// }
+
+
+
+// comparar(numero1, numero2)
+
+// let nacionalidade = "Brasileiro"
+
+// switch (nacionalidade){
+// case "Brasileiro":
+//     console.log("Olá brasileiro")
+//     break
+//     case "Brasileira":
+//     console.log ("olá brasileira")
+//     break
+//     default:
+//         console.log("Você não é do brasil")
+//         break
+
+// }
+
+// const pokemon = Number(prompt(`escolha um pokemon)
+// 1 - Bulbasauro
+// 2 - Squirtle
+// 3 - Charmander
+
+// `))
+
+// switch (pokemon) {
+//     case 1:
+//         console.log("Você escolheu Bulbassauro")
+
+//         break;
+//     case 2:
+//         console.log("Você escolheu Squirtle")
+//         break
+//     case 3:
+//         console.log("Você escolheu o Charmander")
+//         break
+//     default:
+//         console.log("usuário, você pode escolher apenas 3.")
+// }
+
